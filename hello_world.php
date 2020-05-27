a6ecdff5300febce8c81f2d65e4d502bc10e3e6b
glofty1983 <66041366+glofty1983@users.noreply.github.com>
2020-05-27 20:30:09 -0300

    Add files via upload

---
+++ hello_world.php
@@ -0,0 +1,7 @@
+<?php
+
+echo                           "<strong>Meu primeiro programa em PHP seus otarios</strong>";
+
+echo                       " <strong>Aprendendo PHP teste de programa realizado no Linux</strong> "
+
+?>
